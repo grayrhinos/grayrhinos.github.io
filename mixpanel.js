b9b151085de519e2ea82092ddc938e3698f1b6a9
Gray Rhino <55380323+grayrhinos@users.noreply.github.com>
2019-11-07 12:37:18 +0900

    Update mixpanel.js

--- mixpanel.js
+++ mixpanel.js
@@ -1,6 +1,6 @@
 // This fuction executes when the user visits the webpage
 function pageVeiw(){
-
+  mixpanel.track("Page View")
 }
 
 // This function executes when the user clicks Perfromars website button
