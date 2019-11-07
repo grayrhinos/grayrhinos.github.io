553d35b14b6e155f2b57ebad00c6a5287238db80
Gray Rhino <55380323+grayrhinos@users.noreply.github.com>
2019-11-07 12:34:07 +0900

    Create mixpanel.js

---
+++ mixpanel.js
@@ -0,0 +1,9 @@
+// This fuction executes when the user visits the webpage
+function pageVeiw(){
+
+}
+
+// This function executes when the user clicks Perfromars website button
+function buttonClicked(){
+
+}
