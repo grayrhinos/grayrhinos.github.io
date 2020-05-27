3c2a8325f48d4a3e1c24658a363d6acc089a6e21
Gray Rhino <55380323+grayrhinos@users.noreply.github.com>
2020-05-27 16:15:54 +0900

    Delete mixpanel.js

--- mixpanel.js
+++
@@ -1,9 +0,0 @@
-// This fuction executes when the user visits the webpage
-function pageVeiw(){
-  mixpanel.track("Page View")
-}
-
-// This function executes when the user clicks Perfromars website button
-function buttonClicked(){
-
-}
