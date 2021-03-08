428aa3979591e00bc5a11785638cabb08c28cdbb
Gray Rhino <55380323+grayrhinos@users.noreply.github.com>
2021-03-08 18:03:12 +0900

    Update workflow.js

--- workflow.js
+++ workflow.js
@@ -1,7 +1,14 @@
-fetch('https://grayrhinos.github.io/workflow.json')
-.then(function(response) {
-  return response.json();
-})
-.then(function(json) {
-  console.log(json)
-});
+// Sending and receiving data in JSON format using POST method
+//
+var xhr = new XMLHttpRequest();
+var url = "https://grayrhinos.github.io/workflow";
+xhr.open("POST", url, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var json = JSON.parse(xhr.responseText);
+        console.log(json.email + ", " + json.password);
+    }
+};
+var data = JSON.stringify(workflow.json);
+xhr.send(data);
