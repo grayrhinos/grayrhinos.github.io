3f8dddb471616135cf31d26531ae172de56d8eea
Gray Rhino <55380323+grayrhinos@users.noreply.github.com>
2021-03-08 18:33:00 +0900

    Update workflow1.js

--- workflow1.js
+++ workflow1.js
@@ -1,9 +1,20 @@
-// Sending and receiving data in JSON format using POST method
-//
-var xhr = new XMLHttpRequest();
-var url = "https://grayrhinos.github.io/workflow";
-xhr.open("POST", url, true);
-xhr.setRequestHeader("Content-Type", "application/json");
-
-var data = JSON.stringify({ "options": { "field-key1":{ "display1":"value1", "display2":"value2" } } });
-xhr.send(data);
+ $.ajax({
+  type: "POST", // Type of Request
+  url: "https://grayrhinos.github.io/workflow", // Url to Post to
+  data: {
+  "options": {
+    "field-key1":{
+        "display1":"value1",
+        "display2":"value2"
+    },
+    "field-key2":{
+        "display3":"value3",
+        "display4":"value4"
+    }
+  }
+}, // What you are send. Not applicable for GET Requests
+  success: function(success){
+  // Enter code after scucess of request
+ },
+  dataType: JSON // Type of Data you are sending. Would recommend JSON.
+});
